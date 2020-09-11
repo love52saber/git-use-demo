16b3a95e50bd1ba30b0f35c0e5b497ef54eff5b3
gjyang <1457659650@qq.com>
2020-09-11 09:17:29 +0800

    Initial commit

---
+++ src/main/java/com/hedian/gitusedemo/GitUseDemoApplication.java
@@ -0,0 +1,15 @@
+package com.hedian.gitusedemo;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Log
+public class GitUseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitUseDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hedian/gitusedemo/GitUseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hedian.gitusedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitUseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
