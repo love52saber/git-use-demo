88a4c1705032ee1903922ca8a85fa4c6570b974d
gjyang <1457659650@qq.com>
2020-09-11 09:35:12 +0800

    bugFix2

--- src/main/java/com/hedian/gitusedemo/GitUseDemoApplication.java
+++ src/main/java/com/hedian/gitusedemo/GitUseDemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitUseDemoApplication {
 
     public static void main(String[] args) {
-        log.info("111");
+        log.info("bugFix2");
         log.info("bugFix1");
         SpringApplication.run(GitUseDemoApplication.class, args);
     }
