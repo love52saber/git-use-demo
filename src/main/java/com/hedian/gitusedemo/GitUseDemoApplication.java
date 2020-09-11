c652873e7ab0a2b001ee4a1c96167e7aae7d8c5b
gjyang <1457659650@qq.com>
2020-09-11 16:45:20 +0800

    feature/1

--- src/main/java/com/hedian/gitusedemo/GitUseDemoApplication.java
+++ src/main/java/com/hedian/gitusedemo/GitUseDemoApplication.java
@@ -9,12 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitUseDemoApplication {
 
     public static void main(String[] args) {
-        log.info("bugFix2");
         log.info("bugFix1");
-        log.info("feature/1 1");
-        log.info("feature/1 2");
         SpringApplication.run(GitUseDemoApplication.class, args);
-        log.info("bugFix3");
     }
 
 }
